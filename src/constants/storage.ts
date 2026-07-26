8dfa7b68c6ef544a1d839dd7d22c88ff5d81f42c
zVokyo <luisstfm@gmail.com>
2026-07-26 08:58:01 +0000

    centraliza chaves do localStorage

---
+++ src/constants/storage.ts
@@ -0,0 +1,9 @@
+export const STORAGE_KEYS = {
+  loggedIn: "loggedIn",
+  user: "user",
+  theme: "cine-xpace-theme",
+  favorites: "favorites",
+  watchLater: "watchLater",
+  ratings: "ratings",
+  history: "history",
+} as const
